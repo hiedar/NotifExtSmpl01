bb75b5ff036d8031278ffc30e7a08d0e671d2745
檜枝　龍一 <r_hieda@waku-2.com>
2016-11-09 18:44:27 +0900

    * Add tests. * Add a shared scheme.

---
+++ NotifExtSmpl01/StationDataService.swift
@@ -0,0 +1,17 @@
+//
+//  StationDataService.swift
+//  NotifExtSmpl01
+//
+//  Created by 檜枝　龍一 on 2016/11/09.
+//
+//
+
+import Foundation
+import UIKit
+
+class StationDataService: NSObject {
+    // 駅名の配列を返すクラスメソッド
+    class func getHibiyaStations() -> [String] {
+        return ["中目黒駅","恵比寿駅","広尾駅","六本木駅","神谷町駅","霞ケ関駅","日比谷駅","銀座駅","東銀座駅","築地駅","八丁堀駅","茅場町駅","人形町駅","小伝馬町駅","秋葉原駅","仲御徒町駅","上野駅","入谷駅","三ノ輪駅","南千住駅","北千住駅"]
+    }
+}
--- NotifExtSmpl01Tests/NotifExtSmpl01Tests.swift
+++ NotifExtSmpl01Tests/NotifExtSmpl01Tests.swift
@@ -10,27 +10,33 @@ import XCTest
 @testable import NotifExtSmpl01
 
 class NotifExtSmpl01Tests: XCTestCase {
-    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testExampleCase1() {
+        var isExist = false
+        let stations = StationDataService.getHibiyaStations()
+        for station in stations {
+            if station == "秋葉原駅" {
+                isExist = true
+            }
         }
+        XCTAssert(isExist, "日比谷線に秋葉原駅は存在する?")
     }
     
+    func testExampleCase2() {
+        var isExist = false
+        let stations = StationDataService.getHibiyaStations()
+        for station in stations {
+            if station == "北千住駅" {
+                isExist = true
+            }
+        }
+        XCTAssert(isExist, "日比谷線に横浜駅は存在する?")
+    }
 }
